569204943c6a8a2113f96a20dfae28dec95ebc1a
mehmet akyıldız <m.akyildiz23@hotmail.com>
2022-07-07 17:08:56 +0300

    Add project files.

---
+++ RecursiveFonksiyonTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+List<FileInfo> files = DosyaYazdir("C:/Program Files/dotnet");
+foreach (FileInfo file in files)
+    Console.WriteLine(file.FullName);
+
+
+
+List<FileInfo> DosyaYazdir(string path)
+{
+    List<FileInfo> fileInfos = new();
+    DirectoryInfo directoryInfo = new(path);
+    DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+    if (directoryInfos.Any())
+        foreach (DirectoryInfo directory in directoryInfos)
+            fileInfos.AddRange(DosyaYazdir(directory.FullName));
+
+    else
+        fileInfos.AddRange(directoryInfo.GetFiles());
+    return fileInfos;
+}
